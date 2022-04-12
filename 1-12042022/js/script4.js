8cc8b569101a559252d3f1a917441fed0a07248c
GUILHERME ARAUJO FRANCA <guilherme.afranca1@senacsp.edu.br>
2022-04-12 17:31:48 -0300

    estudo de javascript=script de menssagem de alerta;script de solicitação de calculos de operação matematica;script de solicitação de cor;- html para teste de scripts

---
+++ 1-12042022/js/script4.js
@@ -0,0 +1,14 @@
+/*
+o script deve receber um número, processar e exibir uma 
+mensagem dizendo se o número digitado é par ou impar
+*/
+
+var num = prompt("digite um número");
+
+if(num % 2 == 0){
+    alert("este número é par");
+}
+else{
+    alert("este numero é impar");
+}
+
